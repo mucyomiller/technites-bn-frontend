7278ad9428ab56b85a2d6f5ffeaaaa78b8ae9506
Titus Thumbi <gikandi95@gmail.com>
2019-12-12 08:24:31 +0200

    accommodation view fix

--- src/__tests__/components/shared/map/MapContainer.test.js
+++ src/__tests__/components/shared/map/MapContainer.test.js
@@ -1,11 +1,19 @@
-import React from "react"
-import { shallow } from "enzyme";
-import {MapContainer} from "../../../../components/shared/map/MapContainer"
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import MapWrapper, {
+  MapContainer
+} from '../../../../components/shared/map/MapContainer';
 
 describe('Map container', () => {
-    let map = shallow(<MapContainer />)
-    test('should map should render', () => {
-        expect(map.find("InfoWindow").exists()).toBe(true)
-    })
-    
-})
+  let map = shallow(<MapContainer />);
+  test('should map should render', () => {
+    expect(map.find('InfoWindow').exists()).toBe(true);
+  });
+  describe('on clicking the marker', () => {
+    const mapWrapper = mount(<MapWrapper />);
+    test('should ', () => {
+      console.log('mapWrapper.debug()', mapWrapper.debug());
+      //   map.find('Marker').simulate('click');
+    });
+  });
+});
